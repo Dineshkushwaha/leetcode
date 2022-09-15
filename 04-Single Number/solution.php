8062e49d3ea83f91f276ffc1aa50da5dede8f747
Dinesh Kushwaha <dinukush18@gmail.com>
2022-09-15 12:23:31 +0800

    04-added

---
+++ 04-Single Number/solution.php
@@ -0,0 +1,20 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function singleNumber($nums) {
+    $result = 0;
+
+        for($i = 0; $i < count($nums); $i++)
+        {
+            $result ^= $nums[$i];
+        }
+
+        return $result;
+
+    }
+}
+?>
